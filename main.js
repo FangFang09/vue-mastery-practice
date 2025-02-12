e17ac691857e01ea475a6e31f41b727e284cb8ef
fangfang <hyf781222@gmail.com>
2025-02-12 11:53:35 +0800

    L2 done

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "shoes",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ut",
+    };
+  },
+});
