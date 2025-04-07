8c3d26c2aa7293a3bd5053dd3a332cc772f37eeb
fangfang <hyf781222@gmail.com>
2025-04-07 17:41:52 +0800

    feat: L10 completed

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            cart: 0,
-            premium: true
-        }
+  data() {
+    return {
+      cart: [],
+      premium: true,
+    };
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
     },
-    methods: {}
-})
+    removeCart(id) {
+      const index = this.cart.findIndex((item) => item === id);
+
+      if (index !== -1) {
+        this.cart.splice(index, 1);
+      }
+    },
+  },
+});
